64ad79dbe60532ad36a2fced0e9834bc997312af
Steve Kluck <stevekluck@quickenloans.com>
2014-04-28 13:38:20 -0400

    Simplify github service

--- src/Github/GithubService.php
+++ src/Github/GithubService.php
@@ -7,56 +7,22 @@
 
 namespace QL\Hal\Agent\Github;
 
-use Github\Api\Repo;
 use Github\Api\Repository\Contents;
 use Github\Exception\RuntimeException;
-use Github\ResultPager;
 
 class GithubService
 {
-    /**
-     * @var Repo
-     */
-    public $repoApi;
-
     /**
      * @var Contents
      */
     public $contentsApi;
 
     /**
-     * @var ResultPager
-     */
-    private $pager;
-
-    /**
-     * @param Repo $repo
      * @param Contents $contents
-     * @param ResultPager $pager
      */
-    public function __construct(Repo $repo, Contents $contents, ResultPager $pager)
+    public function __construct(Contents $contents)
     {
-        $this->repoApi = $repo;
         $this->contentsApi = $contents;
-        $this->pager = $pager;
-    }
-
-    /**
-     * Get the extended metadata for a repository.
-     *
-     * @param string $user
-     * @param string $repo
-     * @return array|null
-     */
-    public function repository($user, $repo)
-    {
-        try {
-            $repository = $this->repoApi->show($user, $repo);
-        } catch (RuntimeException $e) {
-            $repository = null;
-        }
-
-        return $repository;
     }
 
     /**
