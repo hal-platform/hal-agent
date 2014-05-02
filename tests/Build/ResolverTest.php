29a39c905f21169a04ded6faef5f72cca82e7088
Steve Kluck <stevekluck@quickenloans.com>
2014-05-02 15:00:55 -0400

    Add resolver test

---
+++ tests/Build/ResolverTest.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace QL\Hal\Agent\Build;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+use QL\Hal\Core\Entity\Build;
+use QL\Hal\Core\Entity\Environment;
+use QL\Hal\Core\Entity\Repository;
+
+class ResolverTest extends PHPUnit_Framework_TestCase
+{
+    public function testBuildNotFound()
+    {
+        $logger = new Logger;
+        $repo = Mockery::mock('QL\Hal\Core\Entity\Repository\BuildRepository', [
+            'find' => null
+        ]);
+
+        $action = new Resolver($logger, $repo, 'ENV_PATH');
+
+        $properties = $action('1234');
+        $this->assertNull($properties);
+
+        $message = $logger->messages()[0];
+        $this->assertSame('error', $message[0]);
+        $this->assertSame('Build "1234" could not be found!', $message[1]);
+    }
+
+    public function testBuildNotCorrectStatus()
+    {
+        $build = new Build;
+        $build->setStatus('Poo');
+
+        $logger = new Logger;
+        $repo = Mockery::mock('QL\Hal\Core\Entity\Repository\BuildRepository', [
+            'find' => $build
+        ]);
+
+        $action = new Resolver($logger, $repo, 'ENV_PATH');
+
+        $properties = $action('1234');
+        $this->assertNull($properties);
+
+        $message = $logger->messages()[0];
+        $this->assertSame('info', $message[0]);
+        $this->assertSame('Found build: 1234', $message[1]);
+
+        $message = $logger->messages()[1];
+        $this->assertSame('error', $message[0]);
+        $this->assertSame('Build "1234" has a status of "Poo"! It cannot be rebuilt.', $message[1]);
+    }
+
+    public function testSuccess()
+    {
+        $environment = new Environment;
+        $environment->setKey('envkey');
+
+        $repository = new Repository;
+        $repository->setGithubUser('user1');
+        $repository->setGithubRepo('repo1');
+        $repository->setBuildCmd('derp');
+        $repository->setKey('repokey');
+
+        $build = new Build;
+        $build->setId('1234');
+        $build->setStatus('Waiting');
+        $build->setEnvironment($environment);
+        $build->setRepository($repository);
+        $build->setBranch('master');
+        $build->setCommit('5555');
+
+
+        $expected = [
+            'build' => $build,
+            'buildCommand' => 'derp',
+            'archiveFile' => 'testdir/debug/hal9000-build-1234.tar.gz',
+            'buildPath' => 'testdir/debug/hal9000-build-1234',
+            'buildFile' => 'testdir/debug-archive/hal9000-1234.tar.gz',
+            'githubUser' => 'user1',
+            'githubRepo' => 'repo1',
+            'githubReference' => '5555'
+        ];
+
+        $expectedEnv = [
+            'HOME' => 'testdir/debug-home',
+            'PATH' => 'ENV_PATH',
+            'HAL_BUILDID' => '1234',
+            'HAL_COMMIT' => '5555',
+            'HAL_GITREF' => 'master',
+            'HAL_ENVIRONMENT' => 'envkey',
+            'HAL_REPO' => 'repokey'
+        ];
+
+        $logger = new Logger;
+        $repo = Mockery::mock('QL\Hal\Core\Entity\Repository\BuildRepository', [
+            'find' => $build
+        ]);
+
+        $action = new Resolver($logger, $repo, 'ENV_PATH');
+        $action->setBaseBuildDirectory('testdir');
+
+        $properties = $action('1234');
+        $this->assertSame($expectedEnv, $properties['environmentVariables']);
+
+        unset($properties['environmentVariables']);
+        $this->assertSame($expected, $properties);
+
+    }
+}
