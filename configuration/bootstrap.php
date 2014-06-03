6e4584c7be8f2b9df64492a0bb198eab91530ebc
Steve Kluck <stevekluck@quickenloans.com>
2014-06-03 08:50:11 -0400

    Rebuild bins to use file bootstrap

---
+++ configuration/bootstrap.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace QL\Hal\Agent\Bootstrap;
+
+use QL\Hal\Agent\CachedContainer;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+$root = __DIR__ . '/../';
+require_once $root . '/vendor/autoload.php';
+
+// Set Timezone to UTC
+ini_set('date.timezone', 'UTC');
+date_default_timezone_set('UTC');
+
+function buildDi($root)
+{
+    $container = new ContainerBuilder;
+    $builder = new YamlFileLoader($container, new FileLocator($root));
+    $builder->load('configuration/config.yml');
+    $container->compile();
+
+    return $container;
+}
+
+function dumpDi(ContainerBuilder $container, $class) {
+    $exploded = explode('\\', $class);
+    $config = [
+        'class' => array_pop($exploded),
+        'namespace' => implode('\\', $exploded)
+    ];
+
+    return (new PhpDumper($container))->dump($config);
+}
+
+function getDi($root)
+{
+    if (class_exists('QL\Hal\Agent\CachedContainer')) {
+        $container = new CachedContainer;
+
+        // Force a fresh container in debug mode
+        if ($container->getParameter('debug')) {
+            $container = buildDi($root);
+        }
+
+        return $container;
+    }
+
+    return buildDi($root);
+}
