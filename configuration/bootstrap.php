8883e1cf09338b9e0416f85da2f4d0eaf2d073e8
Steve Kluck <stevekluck@quickenloans.com>
2014-10-07 07:54:32 -0400

    Increase memory limit from default (128M) to 384M

--- configuration/bootstrap.php
+++ configuration/bootstrap.php
@@ -18,6 +18,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // Set Timezone to UTC
 ini_set('date.timezone', 'UTC');
 date_default_timezone_set('UTC');
+ini_set('memory_limit','384M');
 
 function buildDi($root)
 {
