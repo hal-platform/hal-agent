3ffa8fb8c385ab4b0340011f0eabc694fd4ef337
Steve Kluck <stevekluck@quickenloans.com>
2015-03-30 09:48:14 -0400

    Update config for new di util and dynamic dbpass import

--- configuration/bootstrap.php
+++ configuration/bootstrap.php
@@ -7,7 +7,6 @@
 
 namespace QL\Hal\Agent\Bootstrap;
 
-use QL\Hal\Agent\CachedContainer;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
@@ -19,40 +18,3 @@ require_once __DIR__ . '/../vendor/autoload.php';
 ini_set('date.timezone', 'UTC');
 date_default_timezone_set('UTC');
 ini_set('memory_limit','384M');
-
-function buildDi($root)
-{
-    $container = new ContainerBuilder;
-    $builder = new YamlFileLoader($container, new FileLocator($root));
-    $builder->load('configuration/config.yml');
-    $container->compile();
-
-    return $container;
-}
-
-function dumpDi(ContainerBuilder $container, $class) {
-    $exploded = explode('\\', $class);
-    $config = [
-        'class' => array_pop($exploded),
-        'namespace' => implode('\\', $exploded)
-    ];
-
-    return (new PhpDumper($container))->dump($config);
-}
-
-function getDi($root)
-{
-    if (class_exists('QL\Hal\Agent\CachedContainer')) {
-        $container = new CachedContainer;
-
-        // Force a fresh container in debug mode
-        if ($container->getParameter('debug')) {
-            $container = buildDi($root);
-        }
-    } else {
-        $container = buildDi($root);
-    }
-
-    $container->set('root', $root);
-    return $container;
-}
