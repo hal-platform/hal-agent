34b54cc810d3031a22c2a556f59048a9e0923317
Steve Kluck <stevekluck@quickenloans.com>
2017-02-13 15:54:34 -0500

    Fix bad docblocks

--- configuration/bootstrap.php
+++ configuration/bootstrap.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
- *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
- *    is strictly prohibited.
- */
 
 namespace Hal\Agent\Bootstrap;
 
